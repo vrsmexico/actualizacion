d88bb38df1b73b4978158405f1022381e36128ce
vrsmexico <92056572+vrsmexico@users.noreply.github.com>
2022-01-03 20:12:04 -0800

    agregar ip en settings

--- VPEL/settings.py
+++ VPEL/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-gjbkq6hf1ga+vv2h@2vkk0#)cyof5e+2gc_+i4$5^w*pjb1#*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['164.90.132.228']
 
 
 # Application definition
@@ -74,12 +75,23 @@ WSGI_APPLICATION = 'VPEL.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+#if DEBUG:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
+#else:
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vpel',
+        'USER': 'vpel1053',
+        'PASSWORD': 'Jm1053',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
 }
 
 
@@ -118,8 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL='catalogo'
 
